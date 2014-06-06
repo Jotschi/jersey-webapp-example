73386b2a33e8092fe67f8211f3f6ea51120b44ca
Johannes Schüth <j.schueth@gentics.com>
2014-06-06 18:58:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yet another jersey JAX-RS API example
+
+Super simple tiny jersey example webapp that features just a /helloworld demo resource.
---
+++ src/main/java/de/jotschi/example/jersey/rest/DummyResponse.java
@@ -0,0 +1,15 @@
+package de.jotschi.example.jersey.rest;
+
+public class DummyResponse {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
